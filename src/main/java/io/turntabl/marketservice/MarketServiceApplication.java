4226e3cb947628ce9d2d496b66ccd56ca41171ec
Kwame Gyimah <kwame.owusu-gyimah@turntabl.io>
2021-11-24 16:42:38 +0000

    added deployment workflow

--- src/main/java/io/turntabl/marketservice/MarketServiceApplication.java
+++ src/main/java/io/turntabl/marketservice/MarketServiceApplication.java
@@ -2,7 +2,11 @@ package io.turntabl.marketservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MarketServiceApplication {
 
