ea88673347bc63786af1e0cb45d5890801ad0e6f
Kwame Gyimah <kwame.owusu-gyimah@turntabl.io>
2021-11-24 15:03:58 +0000

    first commit

---
+++ src/main/java/io/turntabl/marketservice/MarketServiceApplication.java
@@ -0,0 +1,13 @@
+package io.turntabl.marketservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/turntabl/marketservice/MarketServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.turntabl.marketservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
