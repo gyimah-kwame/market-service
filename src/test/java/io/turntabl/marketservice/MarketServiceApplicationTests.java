35496c04895de3bdb8f6d27a732df94323bf96bc
Kwame Gyimah <kwame.owusu-gyimah@turntabl.io>
2021-11-25 19:34:59 +0000

    updates

--- src/test/java/io/turntabl/marketservice/MarketServiceApplicationTests.java
+++ src/test/java/io/turntabl/marketservice/MarketServiceApplicationTests.java
@@ -1,5 +1,6 @@
 package io.turntabl.marketservice;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class MarketServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.assertTrue(true);
 	}
 
 }
